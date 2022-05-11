ac62c5c1ba7af76c49a5c3bad87ea69c9ffa856c
Maciej Goszczycki <mgoszcz2@gmail.com>
2022-05-11 20:11:59 +0200

    Add sarsa, rename q-learning agent

---
+++ Qlearning.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+# 0: LEFT
+# 1: DOWN
+# 2: RIGHT
+# 3: UP
+
+
+class Qlearning(object):  # Keep the class name!
+    """The world's simplest agent!"""
+
+    def __init__(self, state_space, action_space):
+        self.action_space = action_space
+        self.state_space = state_space
+        self.alpha = 0.7
+        self.gamma = 0.95
+        self.epsilon = 0.05
+        # self.Q = [[0.0]*action_space]*state_space
+        self.Q = np.zeros((state_space, action_space))
+        self.method = "Qlearning"
+        self.state_n = (0, 0)
+        self.last_action = -1
+        self.last_state = 0
+
+    def observe(self, observation, reward, done):
+        self.Q[self.last_state][self.last_action] += self.alpha * (
+            reward
+            + self.gamma * max(self.Q[observation][:])
+            - self.Q[self.last_state][self.last_action]
+        )
+
+    def act(self, observation):
+        return self.Qlearning(observation)
+
+    def Qlearning(self, observation):
+        self.last_state = observation
+        random_action = np.random.randint(self.action_space)
+        max_action = np.argmax(self.Q[observation, :])
+        action = np.random.choice(
+            [max_action, random_action], p=[1 - self.epsilon, self.epsilon]
+        )
+        if np.array_equal(self.Q[observation, :], [0.0, 0.0, 0.0, 0.0]):
+            action = np.random.randint(self.action_space)
+        self.last_action = action
+        return action
---
+++ sarsa.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+# 0: LEFT
+# 1: DOWN
+# 2: RIGHT
+# 3: UP
+
+
+class Agent(object):  # Keep the class name!
+    """Sarsa agent"""
+
+    def __init__(self, state_space, action_space):
+        self.action_space = action_space
+        self.state_space = state_space
+        self.alpha = 0.7
+        self.gamma = 0.95
+        self.epsilon = 0.05
+        self.Q = np.zeros((state_space, action_space))
+        self.state_n = (0, 0)
+        # self.last_action = -1
+        self.last_state = 0
+
+        self.last_action = self.choose_action(0)
+
+    def observe(self, state, reward, done):
+        # state = S'
+        # last-state = S
+        # reward = R
+        # self.action = A
+        next_action = self.choose_action(state)
+        self.Q[self.last_state][self.last_action] += self.alpha * (
+            reward
+            + self.gamma * self.Q[state][next_action]
+            - self.Q[self.last_state][self.last_action]
+        )
+        self.last_action = next_action
+
+    def choose_action(self, state):
+        random_action = np.random.randint(self.action_space)
+        max_action = np.argmax(self.Q[state, :])
+        action = np.random.choice(
+            [max_action, random_action], p=[1 - self.epsilon, self.epsilon]
+        )
+        if np.array_equal(self.Q[state, :], [0.0, 0.0, 0.0, 0.0]):
+            action = np.random.randint(self.action_space)
+        return action
+
+    def act(self, state):
+        self.last_state = state
+        return self.last_action
